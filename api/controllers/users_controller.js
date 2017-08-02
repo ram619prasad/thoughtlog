c2a9c3c88ab83089ef0e1bf54d8cef5eef613c1b
RamPrasadReddy Maddi <ram.prasad@introhive.com>
2017-08-02 22:16:30 +0530

    Thoughtlog: User controller and models.
    * Added route for signup. Need to implement validations,
      and business logic.

---
+++ api/controllers/users_controller.js
@@ -0,0 +1,11 @@
+'use strict'; // Please always write code in strict mode
+
+// import user model
+const User = require('../models/user_model');
+
+const signup = (req, res) => {
+  // console.log(req.body)
+  // res.json({ status: 200, message: 'ok'})
+}
+
+module.exports = { signup }
